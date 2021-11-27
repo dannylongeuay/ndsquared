f66af7d9c433041baec87800defc6f4559a6112a
Daniel Longeuay <dannylongeuay@yahoo.com>
2021-11-26 20:38:10 -0800

    chore: :construction: debug svg color

--- apps/portfolio/src/routes/connectfour.svelte
+++ apps/portfolio/src/routes/connectfour.svelte
@@ -71,7 +71,8 @@
 							setCellFillColor(x, y, currentColor);
 						}}
 					>
-						<svg viewBox="0 0 100 100" fill={cell.fillColor} xmlns="http://www.w3.org/2000/svg">
+						{cell.fillColor}
+						<svg viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
 							<circle cx="50" cy="50" r="50" stroke="black" stroke-width="3" />
 						</svg>
 					</button>
