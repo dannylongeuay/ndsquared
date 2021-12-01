2aa97030df7dcaa7e57b426f8df783c7bd8e04f5
Daniel Longeuay <dannylongeuay@yahoo.com>
2021-11-30 16:32:40 -0800

    chore: :construction: flex test

--- apps/portfolio/src/routes/connectfour.svelte
+++ apps/portfolio/src/routes/connectfour.svelte
@@ -63,10 +63,10 @@
 		<svg height="100" width="100">
 			<circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
 		</svg>
-		<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-			<circle cx="50" cy="50" r="50" fill="blue" />
-		</svg>
 	</div>
+	<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+		<circle cx="50" cy="50" r="50" fill="blue" />
+	</svg>
 	<div class="grid grid-cols-9 bg-base-300">
 		<div class="flex justify-center p-1">
 			<svg height="100" width="100">
