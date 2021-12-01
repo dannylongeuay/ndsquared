43d45d9b35a720685e9ee0aeaf5638156dc21037
Daniel Longeuay <dannylongeuay@yahoo.com>
2021-11-30 16:51:40 -0800

    chore: :construction: removed flex elements from grid

--- apps/portfolio/src/routes/connectfour.svelte
+++ apps/portfolio/src/routes/connectfour.svelte
@@ -58,67 +58,29 @@
 </script>
 
 <div class="container mx-auto">
-	<div class="flex justify-center mb-8">
+	<div class="flex items-center justify-center mb-8">
 		<h1 class="text-4xl md:text-8xl text-secondary-focus">Connect Four</h1>
 		<svg height="100" width="100">
 			<circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
 		</svg>
 	</div>
-	<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-		<circle cx="50" cy="50" r="50" fill="blue" />
-	</svg>
-	<div class="grid grid-cols-9 bg-base-300">
-		<div class="flex justify-center p-1">
-			<svg height="100" width="100">
-				<circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
-			</svg>
-		</div>
-		<div class="flex items-center justify-center p-1">
-			<svg height="50" width="50">
-				<circle cx="25" cy="25" r="25" stroke="black" stroke-width="3" fill="green" />
-			</svg>
-		</div>
-		<div class="flex items-center justify-center p-1">
-			<svg height="20" width="20">
-				<circle cx="10" cy="10" r="10" stroke="black" stroke-width="3" fill="purple" />
-			</svg>
-		</div>
-		<div>
-			<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-				<circle cx="50" cy="50" r="50" fill="blue" />
-			</svg>
-		</div>
-		<div>
-			<svg viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
-				<circle cx="25" cy="25" r="25" fill="magenta" />
-			</svg>
-		</div>
-		<div>
-			<svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-				<circle cx="10" cy="10" r="10" fill="pink" />
-			</svg>
-		</div>
-	</div>
-
-	<div class="grid grid-cols-9 bg-base-300">
-		{#each board as boardRows, y}
-			{#each boardRows as cell, x}
-				<div class="flex justify-center p-1">
-					<svg height="100" width="100">
-						<circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
-					</svg>
-					<!-- <svg
-						viewBox="0 0 100 100"
-						xmlns="http://www.w3.org/2000/svg"
-						on:click={() => {
-							setCellFillColor(x, y, currentColor);
-						}}
-					>
-						<circle cx="50" cy="50" r="50" fill={cell.fillColor} />
-						Unable to render SVG! {cell.fillColor}
-					</svg> -->
-				</div>
+	<div class="flex justify-center">
+		<div class="grid max-w-screen-sm grid-cols-9 bg-base-300 md:max-w-screen-md lg:max-w-screen-lg">
+			{#each board as boardRows, y}
+				{#each boardRows as cell, x}
+					<div class="p-1">
+						<svg
+							viewBox="0 0 100 100"
+							xmlns="http://www.w3.org/2000/svg"
+							on:click={() => {
+								setCellFillColor(x, y, currentColor);
+							}}
+						>
+							<circle cx="50" cy="50" r="50" fill={cell.fillColor} />
+						</svg>
+					</div>
+				{/each}
 			{/each}
-		{/each}
+		</div>
 	</div>
 </div>
