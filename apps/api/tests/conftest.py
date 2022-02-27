4a0ed71a165c6fa2c12185473e560218c5271e43
dannylongeuay <dannylongeuay@yahoo.com>
2022-02-27 00:02:06 -0800

    test: :white_check_mark: api and add code quality tools

---
+++ apps/api/tests/conftest.py
@@ -0,0 +1,26 @@
+import pytest
+
+from dramatiq import Worker
+from fastapi.testclient import TestClient
+
+from app.main import app, broker
+
+
+@pytest.fixture()
+def stub_broker():
+    broker.flush_all()
+    return broker
+
+
+@pytest.fixture()
+def stub_worker():
+    worker = Worker(broker, worker_timeout=100)
+    worker.start()
+    yield worker
+    worker.stop()
+
+
+@pytest.fixture()
+def client():
+    client = TestClient(app)
+    return client
---
+++ apps/api/tests/test_main.py
@@ -0,0 +1,21 @@
+from fastapi.testclient import TestClient
+
+from app.main import DelayRequest, delay_actor
+
+
+def test_ping(client: TestClient):
+    resp = client.get("/ping")
+    assert resp.status_code == 200
+    assert resp.json() == {"message": "pong"}
+
+
+def test_delay(client: TestClient, stub_broker, stub_worker):
+    req = DelayRequest(amount=1)
+    resp = client.post(
+        "/delay",
+        json=req.dict(),
+    )
+    stub_broker.join(delay_actor.queue_name)
+    stub_worker.join()
+    assert resp.status_code == 200
+    assert resp.json() == {"message": "Delayed for 1 second(s)"}
